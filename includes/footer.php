ff478d6047c77c4609a6bf7d29f9ef1807f9d2c3
varunbehere <varunbehere@gmail.com>
2023-11-22 11:34:28 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php echo date('Y');?> Online Course Registration<br> Submitted as Mini Project By- Varun Behere & Aniket Mogal
+                </div>
+
+            </div>
+        </div>
+    </footer>
